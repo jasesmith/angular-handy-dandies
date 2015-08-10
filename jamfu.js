5ce8ce9959a66585257c84145ac2e8e31d2780d2
Jase <jase@star29.com>
2015-08-10 12:05:15 -0600

    added utility service

--- jamfu.js
+++ jamfu.js
@@ -279,7 +279,6 @@ angular.module('jamfu', [])
         }
     };
 }])
-
 .directive('scrollTo', ['$timeout', function($timeout) {
     return {
         restrict: 'A',
@@ -483,6 +482,170 @@ angular.module('jamfu', [])
             localStorage.clear();
         }
     };
+})
+
+.factory('UtilityService', function(){
+    var _keyCharList = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var _hexCharList = '0123456789ABCDEF';
+
+    var randomString = function(len, chars){
+        var result = '';
+        for (var i = len; i > 0; --i) {
+            result += chars[Math.round(Math.random() * (chars.length - 1))];
+        }
+        return result;
+    };
+
+    var createUuid = function(){
+        return randomString(12, _keyCharList) + '-' + randomString(12, _keyCharList);
+    };
+
+    var randomColor = function(sample){
+        sample = sample || $rootScope.colors;
+        return _.sample(sample);
+    };
+
+    var randomHexColor = function() {
+        var hex = '#';
+        for (var i = 0; i < 6; i++ ) {
+            hex += _hexCharList.charAt(Math.floor(Math.random() * _hexCharList.length));
+        }
+        return hex;
+    };
+
+    var colorBrightness = function(color){
+        var r, g, b, brightness;
+
+        if (color.match(/^rgb/)) {
+            color = color.match(/rgb\(([^)]+)\)/)[1];
+            color = color.split(/ *, */).map(Number);
+            r = color[0];
+            g = color[1];
+            b = color[2];
+        } else if ('#' == color[0] && 7 == color.length) {
+            r = parseInt(color.slice(1, 3), 16);
+            g = parseInt(color.slice(3, 5), 16);
+            b = parseInt(color.slice(5, 7), 16);
+        } else if ('#' == color[0] && 4 == color.length) {
+            r = parseInt(color[1] + color[1], 16);
+            g = parseInt(color[2] + color[2], 16);
+            b = parseInt(color[3] + color[3], 16);
+        }
+
+        brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+        return brightness;
+    };
+
+    var numbersArray = function(num, math){
+        if(!math) {math = 0;}
+        return new Array(num + math);
+    };
+
+    var getNumbers = function(target){
+        var numbers = {};
+        if(target) {
+            numbers = {
+                t: target.offsetTop,
+                r: target.offsetLeft + target.offsetWidth,
+                b: target.offsetTop + target.offsetHeight,
+                l: target.offsetLeft,
+                w: target.offsetWidth,
+                h: target.offsetHeight,
+            };
+            // find x|y center
+            numbers.cx = (numbers.l + (numbers.w/2));
+            numbers.cy = (numbers.t + (numbers.h/2));
+        }
+        return numbers;
+    };
+
+    var getMetrics = function(el, parent, rtl, factor) {
+        rtl = rtl || false;
+        factor = factor || false;
+
+        var en = getNumbers(el);
+        var pn = getNumbers(parent);
+
+        var p1 = {
+            x: en.cx,
+            y: en.cy
+        };
+        var p2 = {
+            x: pn.cx,
+            y: pn.cy
+        };
+
+        // angle in radians
+        var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+
+        // angle in degrees
+        var angleDegrees = angleRadians * 180 / Math.PI;
+
+        if(rtl) {
+            // this because if the line's transform-origin is right instead left
+            angleDegrees -= 180;
+        }
+
+        // length of line between two points
+        // last operation as this alters the number set
+        var lineLength = Math.sqrt(((p1.x -= p2.x) * p1.x) + ((p1.y -= p2.y) * p1.y));
+
+        if(factor) {
+            lineLength = lineLength*factor;
+        }
+
+        return {
+            lineLength: lineLength,
+            angleRadians: angleRadians,
+            angleDegrees: angleDegrees
+        };
+    };
+
+    var findDeep = function(collection, id, found, action) {
+        found = found || {item: undefined, parentIds: []};
+
+        var item;
+        item = _.find(collection, function(cell, index) {
+            return cell.id === id;
+        });
+        if (item) {
+            found.item = $angular.copy(item);
+            found.parentIds = [];
+            if(action === 'remove') {
+                var index = collection.indexOf(id);
+                if(index > -1) {
+                    collection.splice(index, 1);
+                }
+            }
+        } else {
+            // if no match is found, search one level deeper for each item
+            for (var i = 0; i < collection.length; i++) {
+                item = collection[i];
+               if (!_.isEmpty(item.children)) {
+                    found = findDeep(item.children, id, found, action);
+                    if (found.item !== undefined) {
+                        found.parentIds.unshift(item.id);
+                        break;
+                    }
+                }
+            }
+        }
+        return found;
+    };
+
+    return {
+        createUuid: createUuid,
+        randomString: randomString,
+        randomColor: randomColor,
+        randomHexColor: randomHexColor,
+        colorBrightness: colorBrightness,
+        numbersArray: numbersArray,
+        getNumbers: getNumbers,
+        getMetrics: getMetrics,
+        findDeep: findDeep
+    }
+
 })
 
 // FILTERS
